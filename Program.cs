fe6198c8afe94f5a91eafdcd1b384635d5a4aa4c
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-11 14:32:58 -0300

    Start Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Rental_of_Vehicles.Entities;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("ENTER RENTAL DATA");
+        Console.Write("Vehicle model: ");
+        string model = Console.ReadLine();
+
+        Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+        DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+        Console.Write("Return (dd/MM/yyyy hh:mm): ");
+        DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+        Rental rental = new Rental(start, finish, new Vehicle(model));
+
+    }
+}
